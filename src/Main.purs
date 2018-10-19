fdb683caa15e333c3b1f0afaff5952331b2c3fe7
Armand Adroher <aadroher@gmail.com>
2018-10-19 09:39:14 +0100

    WIP

--- src/Main.purs
+++ src/Main.purs
@@ -1,49 +1,40 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
+
 import Data.Maybe (fromJust)
--- import Effect.Console (log)
-
-import Web.HTML (window) as DOM
-import Web.HTML.Window (document) as DOM
-import Web.HTML.HTMLDocument (toNonElementParentNode) as DOM
-import Web.DOM.NonElementParentNode (getElementById) as DOM
-
-import React as R
-import React.DOM as RD
--- import React.DOM.Props as RP
-import ReactDOM as RDOM
-
+import Effect (Effect)
 import Partial.Unsafe (unsafePartial)
+-- import React (pureComponent)
+import React as R
+import React.DOM as D
+import ReactDOM as RDOM
+import Web.DOM.NonElementParentNode (getElementById) as DOM
+import Web.HTML (window) as DOM
+import Web.HTML.HTMLDocument (toNonElementParentNode) as DOM
+import Web.HTML.Window (document) as DOM
 
--- import Thermite as T
+appElementId :: String
+appElementId = "app"
 
 app :: R.ReactClass { }
-app = R.pureComponent "App" component
-  where 
-  component this =
-    pure { state: { }
-         , render: render
-         }
-    where
-    render =
-        RD.div 
+app = R.statelessComponent "App" $ \_ ->
+        D.div 
           [] 
-          [ RD.h1
+          [ D.h1
             []
-            [ RD.text "Armand Adroher Salvia"
+            [ D.text "Armand Adroher Salvia"
             ]
           ]
 
 main :: Effect Unit
-main = void $ do
+main = void do
   window <- DOM.window
   document <- DOM.document window
   let node = DOM.toNonElementParentNode document
   appElement <- DOM.getElementById "app" node
-  let appElement' = unsafePartial (fromJust appElement)
-  RDOM.render (R.createLeafElement app { }) appElement'
+  RDOM.render (R.createLeafElement app { }) (unsafePartial fromJust appElement)
+  
 
 
   
