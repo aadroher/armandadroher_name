ff41762eaf1787f9a933090126953c1983a45e75
Armand Adroher <aadroher@gmail.com>
2018-11-20 01:56:31 +0000

    Gets first react rendering to work.

--- src/Main.purs
+++ src/Main.purs
@@ -1,49 +1,24 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
+
 import Data.Maybe (fromJust)
--- import Effect.Console (log)
-
-import Web.HTML (window) as DOM
-import Web.HTML.Window (document) as DOM
-import Web.HTML.HTMLDocument (toNonElementParentNode) as DOM
-import Web.DOM.NonElementParentNode (getElementById) as DOM
-
-import React as R
-import React.DOM as RD
--- import React.DOM.Props as RP
-import ReactDOM as RDOM
-
+import Effect (Effect)
+import Effect.Console (log)
 import Partial.Unsafe (unsafePartial)
-
--- import Thermite as T
-
-app :: R.ReactClass { }
-app = R.pureComponent "App" component
-  where 
-  component this =
-    pure { state: { }
-         , render: render
-         }
-    where
-    render =
-        RD.div 
-          [] 
-          [ RD.h1
-            []
-            [ RD.text "Armand Adroher Salvia"
-            ]
-          ]
+import React.DOM as D
+import ReactDOM (render)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
-main = void $ do
-  window <- DOM.window
-  document <- DOM.document window
-  let node = DOM.toNonElementParentNode document
-  appElement <- DOM.getElementById "app" node
-  let appElement' = unsafePartial (fromJust appElement)
-  RDOM.render (R.createLeafElement app { }) appElement'
-
+main = void do 
+  log "Rendering armandadroher.name"
+  let component = D.div [] [ D.text "Hello, sailor" ]
+  doc <- window >>= document
+  containter <- getElementById "app" (toNonElementParentNode doc)
+  render component (unsafePartial fromJust containter)
 
   
