c96eaaec0c6e7be8424344ca99ec8194ced91357
Armand Adroher <aadroher@gmail.com>
2018-12-21 00:27:12 +0000

    First new animated green style.

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,11 @@ const Header = ({ className }: { className: string }) => (
   <h1 className={className}>Armand Adroher Salvia</h1>
 );
 
-type BlurRadiusStyle = {
-  textShadow: string;
-};
 type App = React.FunctionComponent<{
   className?: string;
-  style: BlurRadiusStyle;
 }>;
-const App: App = ({ className, style }) => (
-  <div className={classnames("app", className)} style={style}>
+const App: App = ({ className }) => (
+  <div className={classnames("app", className)}>
     <Layout>
       <Header className="header" />
       <p>Software engineer and school teacher</p>
@@ -84,70 +80,87 @@ const StyledApp = styled(App)`
   }
 `;
 
-class DynamicApp extends React.Component<{}> {
-  maxBlurRadius = 2;
-  minBlurRadius = 1;
-  blurRadiusChangeStep = 0.01;
-  // blurRadiusUpdateDelay = 10; //ms
+// class DynamicApp extends React.Component<{}> {
+//   maxBlurRadius = 2;
+//   minBlurRadius = 1;
+//   blurRadiusChangeStep = 0.01;
+//   // blurRadiusUpdateDelay = 10; //ms
 
-  state: {
-    blurAnimationOn: boolean;
-    blurRadiusDirection: number;
-    blurRadius: number;
-  };
+//   state: {
+//     blurAnimationOn: boolean;
+//     blurRadiusDirection: number;
+//     blurRadius: number;
+//   };
 
-  intervalId: undefined | number;
+//   intervalId: undefined | number;
 
-  constructor(props: {}) {
-    super(props);
+//   constructor(props: {}) {
+//     super(props);
 
-    this.state = {
-      blurAnimationOn: true,
-      blurRadiusDirection: 1,
-      blurRadius: 1
-    };
+//     this.state = {
+//       blurAnimationOn: false,
+//       blurRadiusDirection: 1,
+//       blurRadius: 1
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.animateBlurRadius();
+//   }
+
+//   componentWillUnmount() {
+//     window.clearInterval(this.intervalId);
+//   }
+
+//   animateBlurRadius = () => {
+//     window.requestAnimationFrame(() => {
+//       this.updateBlurRadius();
+//       const { blurAnimationOn } = this.state;
+//       if (blurAnimationOn) {
+//         this.animateBlurRadius();
+//       }
+//     });
+//   };
+
+//   updateBlurRadius = () => {
+//     const { blurRadius, blurRadiusDirection } = this.state;
+//     const shouldChangeDirection =
+//       blurRadius < this.minBlurRadius || this.maxBlurRadius < blurRadius;
+//     const newBlurRadiusDirection = shouldChangeDirection
+//       ? -blurRadiusDirection
+//       : blurRadiusDirection;
+//     const newBlurRadius =
+//       newBlurRadiusDirection * this.blurRadiusChangeStep + blurRadius;
+
+//     this.setState({
+//       blurRadiusDirection: newBlurRadiusDirection,
+//       blurRadius: newBlurRadius
+//     });
+//   };
+
+//   render = () => {
+//     const style = {
+//       textShadow: `${fontColour} 0 0 ${this.state.blurRadius}rem`
+//     };
+//   };
+// }
+
+const AnimatedApp = styled(StyledApp)`
+  @keyframes shadow-blur {
+    from {
+      text-shadow: ${fontColour} 0 0 0.3rem;
+    }
+
+    to {
+      text-shadow: ${fontColour} 0 0 0.4rem;
+    }
   }
 
-  componentDidMount() {
-    this.animateBlurRadius();
-  }
+  animation-name: shadow-blur;
+  animation-duration: 5s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  animation-timing-function: ease-out;
+`;
 
-  componentWillUnmount() {
-    window.clearInterval(this.intervalId);
-  }
-
-  animateBlurRadius = () => {
-    window.requestAnimationFrame(() => {
-      this.updateBlurRadius();
-      const { blurAnimationOn } = this.state;
-      if (blurAnimationOn) {
-        this.animateBlurRadius();
-      }
-    });
-  };
-
-  updateBlurRadius = () => {
-    const { blurRadius, blurRadiusDirection } = this.state;
-    const shouldChangeDirection =
-      blurRadius < this.minBlurRadius || this.maxBlurRadius < blurRadius;
-    const newBlurRadiusDirection = shouldChangeDirection
-      ? -blurRadiusDirection
-      : blurRadiusDirection;
-    const newBlurRadius =
-      newBlurRadiusDirection * this.blurRadiusChangeStep + blurRadius;
-
-    this.setState({
-      blurRadiusDirection: newBlurRadiusDirection,
-      blurRadius: newBlurRadius
-    });
-  };
-
-  render = () => {
-    const style = {
-      textShadow: `${fontColour} 0 0 ${this.state.blurRadius}rem`
-    };
-    return <StyledApp style={style} />;
-  };
-}
-
-export default DynamicApp;
+export default AnimatedApp;
