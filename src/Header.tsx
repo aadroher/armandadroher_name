8af94fc19956659bb937d87687bbb012789dcf12
Armand Adroher <aadroher@gmail.com>
2019-01-12 23:43:17 +0000

    Copies fonts over.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,54 @@
 import React from "react";
 import figlet from "figlet";
-import { strict } from "assert";
+
+type getBanner = (
+  args: { text: string; fontName?: figlet.Fonts }
+) => Promise<string>;
+const getBanner: getBanner = ({ text, fontName }) =>
+  new Promise((resolve: Function, reject: Function) => {
+    figlet(text, { font: fontName }, (err, bannerText) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(bannerText);
+      }
+    });
+  });
 
 interface HeaderProps {
   fontName?: string;
 }
-const Header = ({ fontName }: HeaderProps) => (
-  <pre className="header">Armand Adroher Salvia</pre>
-);
+
+class Header extends React.Component<HeaderProps> {
+  state: {
+    bannerText: string;
+  };
+
+  constructor(props: HeaderProps) {
+    super(props);
+    this.state = {
+      bannerText: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log("Component did mount");
+    const text = "Armand Adroher";
+    getBanner({ text, fontName: "Standard" })
+      .then(bannerText => {
+        console.log("getBanner resolved");
+        console.log(bannerText);
+        this.setState({ bannerText });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    const { bannerText } = this.state;
+    return <pre className="header">{bannerText}</pre>;
+  }
+}
 
 export default Header;
