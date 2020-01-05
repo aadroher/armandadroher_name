19bb76cfba70cf3c3f1ad959c3caa054d3ba10f1
Armand Adroher <aadroher@gmail.com>
2020-01-05 16:34:30 +0000

    Adds basic static rendering.

--- src/game-of-life/game-of-life.tsx
+++ src/game-of-life/game-of-life.tsx
@@ -7,17 +7,46 @@ const initialCoordinates: number[][] = [
   [-1, 0],
 ];
 
+const worldDimentions = {
+  width: 800,
+  height: 600,
+};
+
+type WorldSVG = React.FunctionComponent<{ aliveCells: number[][] }>;
+const WorldSVG: WorldSVG = ({ aliveCells }) => {
+  const { width, height } = worldDimentions;
+  return (
+    <svg
+      version="1.1"
+      baseProfile="full"
+      width="800"
+      height="600"
+      viewBox={`-100 -75 200 150`}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {aliveCells.map(([column, row]) => (
+        <rect
+          key={`${column},${row}`}
+          x={column * 1.1}
+          y={row * 1.1}
+          width="1"
+          height="1"
+          fill="violet"
+          // stroke="#0f0f0f"
+          // strokeWidth="0.1"
+        />
+      ))}
+    </svg>
+  );
+};
+
 type GameOfLife = React.FunctionComponent<{}>;
 const GameOfLife: GameOfLife = () => {
   const canvasRef = useRef(null);
   return (
     <>
       <p>The game of life</p>
-      <canvas
-        ref={canvasRef}
-        width={window.innerWidth}
-        height={window.innerHeight}
-      />
+      <WorldSVG aliveCells={initialCoordinates} />
     </>
   );
 };
