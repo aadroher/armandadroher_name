14ba30c6930df8a496e6c57b9dd53b269dc984b9
Armand Adroher <aadroher@gmail.com>
2020-01-05 11:57:56 +0000

    Adds eslint

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,11 +1,12 @@
+/* eslint-disable */
 /// <reference types="node" />
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'test'
-    PUBLIC_URL: string
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
   }
 }
 
