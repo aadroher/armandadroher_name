620a9e29e7300a70fe541a51550760f750fad0f5
Armand Adroher <aadroher@gmail.com>
2020-01-05 17:08:22 +0000

    Adds basic random cell generation.

--- src/game-of-life/game-of-life.tsx
+++ src/game-of-life/game-of-life.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { createCells, createWorld, evolve } from './world';
 
 const initialCoordinates: number[][] = [
@@ -7,46 +7,72 @@ const initialCoordinates: number[][] = [
   [-1, 0],
 ];
 
-const worldDimentions = {
-  width: 800,
-  height: 600,
+type WorldDimensions = {
+  width: number;
+  height: number;
+};
+const worldDimensions: WorldDimensions = {
+  width: 200,
+  height: 150,
 };
 
-type WorldSVG = React.FunctionComponent<{ aliveCells: number[][] }>;
-const WorldSVG: WorldSVG = ({ aliveCells }) => {
-  const { width, height } = worldDimentions;
-  return (
-    <svg
-      version="1.1"
-      baseProfile="full"
-      width="800"
-      height="600"
-      viewBox={`-100 -75 200 150`}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {aliveCells.map(([column, row]) => (
-        <rect
-          key={`${column},${row}`}
-          x={column * 1.1}
-          y={row * 1.1}
-          width="1"
-          height="1"
-          fill="violet"
-          // stroke="#0f0f0f"
-          // strokeWidth="0.1"
-        />
-      ))}
-    </svg>
-  );
-};
+type GetRandomInt = (range: number) => number;
+const getRandomInt: GetRandomInt = range =>
+  Math.floor(Math.random() * range - range / 2);
+
+type GetRandomCoordinates = (
+  worldDimensions: WorldDimensions,
+  numCells: number
+) => number[][];
+const getRandomCoordinates: GetRandomCoordinates = (
+  { width, height },
+  numCells
+) =>
+  [...Array(numCells).keys()].map(() => [
+    getRandomInt(width),
+    getRandomInt(height),
+  ]);
+
+type WorldSVG = React.FunctionComponent<{
+  aliveCells: number[][];
+  worldDimensions: WorldDimensions;
+}>;
+const WorldSVG: WorldSVG = ({
+  aliveCells,
+  worldDimensions: { width, height },
+}) => (
+  <svg
+    version="1.1"
+    baseProfile="full"
+    width="800"
+    height="600"
+    viewBox={[-width / 2, -height / 2, width, height].join(' ')}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    {aliveCells.map(([column, row]) => (
+      <rect
+        key={`${column},${row}`}
+        x={column * 1.1}
+        y={row * 1.1}
+        width="1"
+        height="1"
+        fill="violet"
+        // stroke="#0f0f0f"
+        // strokeWidth="0.1"
+      />
+    ))}
+  </svg>
+);
 
 type GameOfLife = React.FunctionComponent<{}>;
 const GameOfLife: GameOfLife = () => {
-  const canvasRef = useRef(null);
+  const [aliveCells, setAliveCells] = useState(
+    getRandomCoordinates(worldDimensions, 2000)
+  );
   return (
     <>
       <p>The game of life</p>
-      <WorldSVG aliveCells={initialCoordinates} />
+      <WorldSVG aliveCells={aliveCells} worldDimensions={worldDimensions} />
     </>
   );
 };
