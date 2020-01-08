ef6bf00e5b18d43620f74221f3c7a053bdea4578
Armand Adroher <aadroher@gmail.com>
2020-01-08 23:31:52 +0000

    WIP

--- src/game-of-life/config.tsx
+++ src/game-of-life/config.tsx
@@ -2,7 +2,9 @@ export type WorldDimensions = {
   width: number;
   height: number;
 };
-export const ticksPerSecond = 12;
+export const baseCellWidth = 1;
+export const baseShadowBlurLength = 0;
+export const ticksPerSecond = 20;
 export const firstTickToRender = 2;
-export const frameToCanvasScaleFactor = 6;
-export const aliveCellDensity = 0.075;
+export const frameToCanvasScaleFactor = 3;
+export const aliveCellDensity = 0.3;
--- src/game-of-life/world-canvas.tsx
+++ src/game-of-life/world-canvas.tsx
@@ -1,17 +1,37 @@
 import React, { useRef, useEffect } from 'react';
 import {
   firstTickToRender,
+  baseCellWidth,
+  baseShadowBlurLength,
   frameToCanvasScaleFactor,
   WorldDimensions,
 } from './config';
 
+const hiddenCanvasWidth =
+  (baseCellWidth + 2 * baseShadowBlurLength) * frameToCanvasScaleFactor;
+
+type HiddenCanvas = React.ForwardRefExoticComponent<
+  React.RefAttributes<HTMLCanvasElement>
+>;
+const HiddenCanvas: HiddenCanvas = React.forwardRef<HTMLCanvasElement, any>(
+  (_, hiddenCanvasRef: React.Ref<HTMLCanvasElement>) => (
+    <canvas
+      ref={hiddenCanvasRef}
+      width={hiddenCanvasWidth}
+      height={hiddenCanvasWidth}
+    />
+  )
+);
+
 type DrawFrame = (
+  hiddenCanvas: HTMLCanvasElement,
   canvas: HTMLCanvasElement,
   canvasContext: CanvasRenderingContext2D,
   worldDimensions: WorldDimensions,
   newAliveCells: number[][]
 ) => void;
 const drawFrame: DrawFrame = (
+  hiddenCanvas,
   canvas,
   context,
   { width, height },
@@ -24,40 +44,52 @@ const drawFrame: DrawFrame = (
     context.lineWidth = 1;
     context.strokeRect(0, 0, canvas.width, canvas.height);
     newAliveCells.forEach(([column, row]) => {
-      const x = (Math.floor(width / 2) + column) * frameToCanvasScaleFactor;
-      const y = (Math.floor(height / 2) - row) * frameToCanvasScaleFactor;
+      const x = Math.floor(
+        (Math.floor(width / 2) + column) * frameToCanvasScaleFactor
+      );
+      const y = Math.floor(
+        (Math.floor(height / 2) - row) * frameToCanvasScaleFactor
+      );
 
-      context.strokeStyle = 'black';
-      context.lineWidth = 0.1 * frameToCanvasScaleFactor;
-      context.fillStyle = 'violet';
-      context.fillRect(
-        x,
-        y,
-        1 * frameToCanvasScaleFactor,
-        1 * frameToCanvasScaleFactor
-      );
-      context.shadowBlur = 0.5 * frameToCanvasScaleFactor;
-      context.shadowColor = 'violet';
-      context.strokeRect(
-        x,
-        y,
-        1 * frameToCanvasScaleFactor,
-        1 * frameToCanvasScaleFactor
-      );
+      context.drawImage(hiddenCanvas, x, y);
+
+      // context.strokeStyle = 'black';
+      // context.lineWidth = Math.floor(0.1 * frameToCanvasScaleFactor);
+      // context.fillStyle = 'violet';
+      // context.fillRect(
+      //   x,
+      //   y,
+      //   1 * frameToCanvasScaleFactor,
+      //   1 * frameToCanvasScaleFactor
+      // );
+      // // context.shadowBlur = Math.floor(0.5 * frameToCanvasScaleFactor);
+      // // context.shadowColor = 'violet';
+      // context.strokeRect(
+      //   x,
+      //   y,
+      //   1 * frameToCanvasScaleFactor,
+      //   1 * frameToCanvasScaleFactor
+      // );
     });
   });
 };
 
 type UpdateCanvas = (
+  hiddenCanvas: HTMLCanvasElement | null,
   canvas: HTMLCanvasElement | null,
   worldDimensions: WorldDimensions,
   newAliveCells: number[][]
 ) => void;
-const updateCanvas: UpdateCanvas = (canvas, worldDimensions, newAliveCells) => {
-  if (canvas) {
+const updateCanvas: UpdateCanvas = (
+  hiddenCanvas,
+  canvas,
+  worldDimensions,
+  newAliveCells
+) => {
+  if (canvas && hiddenCanvas) {
     const context = canvas.getContext('2d', { alpha: false });
     if (context) {
-      drawFrame(canvas, context, worldDimensions, newAliveCells);
+      drawFrame(hiddenCanvas, canvas, context, worldDimensions, newAliveCells);
     }
   }
 };
@@ -68,18 +100,69 @@ type WorldCanvas = React.FunctionComponent<{
   worldDimensions: WorldDimensions;
 }>;
 const WorldCanvas: WorldCanvas = ({ tick, aliveCells, worldDimensions }) => {
-  const canvasRef = useRef(null);
+  const hiddenCanvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  let hiddenCanvas: HTMLCanvasElement | null;
+
+  useEffect(() => {
+    hiddenCanvas = hiddenCanvasRef.current;
+    if (hiddenCanvas && tick <= firstTickToRender) {
+      const context: CanvasRenderingContext2D | null = hiddenCanvas.getContext(
+        '2d'
+      );
+      console.log({ context });
+      if (context) {
+        console.log('Draw in hidden canvas.');
+        context.strokeStyle = 'black';
+        context.lineWidth = Math.floor(0.1 * frameToCanvasScaleFactor);
+        context.fillStyle = 'violet';
+        // context.shadowBlur = baseShadowBlurLength * frameToCanvasScaleFactor;
+        // context.shadowColor = 'violet';
+        // const x = baseShadowBlurLength * frameToCanvasScaleFactor;
+        // const y = baseShadowBlurLength * frameToCanvasScaleFactor;
+        const x = 0;
+        const y = 0;
+        console.log({
+          baseCellWidth: baseCellWidth * frameToCanvasScaleFactor,
+          x,
+          y,
+        });
+        context.fillRect(
+          x,
+          y,
+          baseCellWidth * frameToCanvasScaleFactor,
+          baseCellWidth * frameToCanvasScaleFactor
+        );
+
+        // context.strokeRect(
+        //   x,
+        //   y,
+        //   baseCellWidth * frameToCanvasScaleFactor,
+        //   baseCellWidth * frameToCanvasScaleFactor
+        // );
+      }
+    }
+  }, [tick]);
+
   useEffect(() => {
     if (canvasRef.current && tick >= firstTickToRender) {
-      updateCanvas(canvasRef.current, worldDimensions, aliveCells);
+      updateCanvas(
+        hiddenCanvasRef.current,
+        canvasRef.current,
+        worldDimensions,
+        aliveCells
+      );
     }
   }, [tick]);
   return (
-    <canvas
-      ref={canvasRef}
-      width={worldDimensions.width * frameToCanvasScaleFactor}
-      height={worldDimensions.height * frameToCanvasScaleFactor}
-    />
+    <>
+      <HiddenCanvas ref={hiddenCanvasRef} />
+      <canvas
+        ref={canvasRef}
+        width={worldDimensions.width * frameToCanvasScaleFactor}
+        height={worldDimensions.height * frameToCanvasScaleFactor}
+      />
+    </>
   );
 };
 
