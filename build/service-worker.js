ae6c11353716fbb58405c58596d75503ea4b1282
Armand Adroher <aadroher@gmail.com>
2020-11-08 11:27:59 +0100

    Commits build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5559734dc534ba1cea80c8bc6b9b49ea.js"
+  "/precache-manifest.3029a3f8bc957ac8c4d070d2b02a224b.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
